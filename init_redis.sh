dce157495702008ea95624323042ccf8c3851f87
yunkun666 <30587900+yunkun666@users.noreply.github.com>
2018-09-08 22:58:49 +0800

    Update init_redis.sh

--- init_redis.sh
+++ init_redis.sh
@@ -13,10 +13,13 @@ sleep 1;
 
 echo 'Installation begins...'
 sed -i -e 's|bind 127.0.0.1|# bind 127.0.0.1|' /etc/redis.conf;
-sed -i -e 's|# requirepass foobared|requirepass 123456|' /etc/redis.conf;
 sed -i -e 's|protected-mode yes|protected-mode no|' /etc/redis.conf;
+read -p "enter passwd:" no
+sed -i -e 's|# requirepass foobared|requirepass $no|' /etc/redis.conf;
 echo 'Congratulations on personalized configuration!';
 sleep 1;
+
+
 
 echo 'start redis......';
 service redis restart;
