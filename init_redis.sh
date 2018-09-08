69c8a0d6883f9b6fa061214bbae744e881beee31
yunkun666 <30587900+yunkun666@users.noreply.github.com>
2018-09-08 21:49:04 +0800

    Update init_redis.sh

--- init_redis.sh
+++ init_redis.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-yum install epel-release;
-yum install redis;
+#yum install epel-release;
+#yum install redis;
+
+sed -i -e 's|bind 127.0.0.1|# bind 127.0.0.1|' /etc/redis.conf;
+echo '启动redis';
+service redis start;
 
